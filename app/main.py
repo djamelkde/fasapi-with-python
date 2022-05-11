ecdba33a581c0fd8cf5d7aa363f078afe6d8b8f8
djamelkde Djamel <djamelkde@gmail.com>
2022-05-11 14:56:38 +0200

    configure CORS option

--- app/main.py
+++ app/main.py
@@ -3,10 +3,23 @@ from .database import engine
 from . import models
 from .routers import user, post, auth, like
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+#CORS domains authorized to connect to our API
+#origins = ["https://www.google.com", "https://www.youtube.com"]
+origins = ["*"] #open for public
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #include the routers
 app.include_router(post.router)
