ff2943640fde3ba29e26a747019f3a27a3277b1b
djamelkde Djamel <djamelkde@gmail.com>
2022-05-14 03:39:19 +0200

    adding postgresql database service in the workfow

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ from app import models
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
